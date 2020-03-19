0d49f11eb642d7aa239e2aaeefc0cd271d30676f
ComarniceanuG <comarniceanugeorge@gmail.com>
2020-03-19 19:29:47 +0200

    Updates

---
+++ precache-manifest.6e117e7c37f2d220c045c81c65dfecf6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2aff337e7ec913ab519995017b7f5a6d",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "1ba983a4f20c10d98d01",
+    "url": "/monsters-rolodex/static/css/main.2add0695.chunk.css"
+  },
+  {
+    "revision": "30436d15d072549122d4",
+    "url": "/monsters-rolodex/static/js/2.0d232ef2.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/monsters-rolodex/static/js/2.0d232ef2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1ba983a4f20c10d98d01",
+    "url": "/monsters-rolodex/static/js/main.769c3b1a.chunk.js"
+  },
+  {
+    "revision": "9e014af7b12dc10ef867",
+    "url": "/monsters-rolodex/static/js/runtime-main.ff038b7c.js"
+  }
+]);
